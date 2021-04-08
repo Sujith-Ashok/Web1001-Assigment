1a2a6236742d38f57a0889b845b05668394a9466
sujithashok <A00218559@mycambrian.ca>
2021-04-08 16:30:11 -0400

    New Data content

---
+++ WebAssignemnt/Context/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WebAssignemnt.Models;
+
+namespace WebAssignemnt.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<AuthorModel> Authors { get; set; }
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
